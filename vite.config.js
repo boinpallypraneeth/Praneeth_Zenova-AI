c77aac2e088e9ce946a51bbf2c80da1ca5f3c555
Praneeth Boinpally <62898042+boinpallypraneeth@users.noreply.github.com>
2025-03-03 06:20:09 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/ZenovaAI/",
+  base: "Praneeth_Zenova-AI",
   plugins: [react()]
 });
